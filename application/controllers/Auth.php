f62f656c0b3d5e19dd5d16ed6d655c22cc44050d
arifnurrizqi <arifnurrizqi@gmail.com>
2024-07-24 09:34:54 +0700

    fix favicon login form

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -7,10 +7,12 @@ class Auth extends CI_Controller
 		show_404();
 	}
 
-	public function login()	{
+	public function login()
+	{
 		// Menampilkan CAPTCHA
 		$this->load->helper('captcha');
 		$this->load->model('landing_model');
+		$this->load->model('identitas_model');
 		$this->config->load('captcha_config');
 
 		$captcha_config = $this->config->item('captcha');
@@ -19,12 +21,14 @@ class Auth extends CI_Controller
 		// Simpan nilai CAPTCHA di session untuk validasi nanti
 		$this->session->set_userdata('captcha', $captcha['word']);
 		$data['captcha_image'] = $captcha['image'];
+		$data['profile'] = $this->identitas_model->get();
 		$data['landing'] = $this->landing_model->getLandingBy('status', 'true');
 
 		$this->load->view('auth/login_form', $data);
 	}
 
-	public function validate_captcha() {
+	public function validate_captcha()
+	{
 		$this->load->model('auth_model');
 		$this->load->library('form_validation');
 
@@ -33,11 +37,11 @@ class Auth extends CI_Controller
 
 		// Mendapatkan nilai CAPTCHA dari session
 		$captcha_session = $this->session->userdata('captcha');
-		
+
 		if ($this->form_validation->run() == FALSE) {
 			return $this->load->view('auth/login_form', $data);
 		}
-		
+
 		$username = $this->input->post('username');
 		$password = $this->input->post('password');
 		$captcha_input = $this->input->post('captcha');
@@ -50,14 +54,15 @@ class Auth extends CI_Controller
 				// CAPTCHA salah
 				$this->session->set_flashdata('message_login_error', ' Security Code salah, pastikan memasukan dengan benar!');
 				redirect(site_url('auth/login'));
-		}
+			}
 		} else {
 			$this->session->set_flashdata('message_login_error', ' Login Gagal, pastikan username dan passwrod benar!');
 			redirect(site_url('auth/login'));
 		}
 	}
-	
-	public function logout() {
+
+	public function logout()
+	{
 		$this->load->model('auth_model');
 		$this->auth_model->logout();
 		redirect(site_url('auth/login'));
